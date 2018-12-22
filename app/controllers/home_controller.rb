6dd71ec886ea153de5d10d108d665e5f94239582
lucas <linkbolto@gmail.com>
2018-12-22 17:24:16 -0200

    Administrador vê transações do dia

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
-    if params[:filter]
-      @transactions = Transaction.where(currency: params[:filter])
-    else
+    case params[:filter]
+    when nil
+      @transactions = Transaction.where(created_at: Date.today.all_day)
+    when 'all'
       @transactions = Transaction.all
+    else
+      @transactions = Transaction.where(currency: params[:filter])
     end
   end
 end
